570ebe313850844c6ce835f111bbeb5c683767a4
Suraj <surajkashyap1996@gmail.com>
2020-10-07 15:08:03 +0530

    migrated database to postgres

--- school_system/settings.py
+++ school_system/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'school_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'school',
+        'USER': 'suraj',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
